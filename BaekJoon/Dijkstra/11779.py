502962b8b15d2508894c8e01fada86de3fdece1a
seheonnn <ho78901@naver.com>
2024-06-19 10:06:38 +0900

    update

--- BaekJoon/Dijkstra/11779.py
+++ BaekJoon/Dijkstra/11779.py
@@ -9,6 +9,7 @@ INF = 999999
 graph = [[] for _ in range(n + 1)]
 
 d = [INF] * (n + 1)
+path = [-1] * (n + 1)
 
 for _ in range(m):
     u, v, w = map(int, sys.stdin.readline().split())
@@ -27,10 +28,9 @@ def dijkstra(s):
         for i in graph[now]:
             if dist + i[1] < d[i[0]]:
                 d[i[0]] = dist + i[1]
+                path[i[0]] = now # 최소 비용 경로를 업데이트
                 heapq.heappush(q, (dist + i[1], i[0]))
 
 
 dijkstra(start)
 print(d[end])
-print("d:", d)
-print(graph)
