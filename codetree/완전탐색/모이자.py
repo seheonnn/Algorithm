9c71c6d9ec56634a360c6e627184f2b30bc830e8
seheonnn <ho78901@naver.com>
2024-07-19 16:01:29 +0900

    완전탐색 - 모이색

---
+++ codetree/완전탐색/모이자.py
@@ -0,0 +1,17 @@
+# 완전탐색
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+
+m = sys.maxsize
+
+for i in range(n):
+    sum = 0
+    for j in range(n):
+        sum += abs(i-j) * lst[j]
+    if m > sum:
+        m = sum
+print(m)
