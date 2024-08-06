ec5fce0a70e9a9a90bf1e96cf851ff6c0e9236a6
seheonnn <ho78901@naver.com>
2024-08-06 22:25:46 +0900

    BFS - 마을구분하기 복습

---
+++ codetree/BFS/마을 구분하기V2.py
@@ -0,0 +1,39 @@
+# 인접행렬 BFS 복습
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
+
+dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+
+def bfs(x, y):
+    global cnt
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or n <= nx or ny < 0 or n <= ny:
+                continue
+            if graph[nx][ny] == 1 and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                queue.append((nx, ny))
+                cnt += 1
+
+result = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            cnt = 1
+            visited[i][j] = 1
+            bfs(i, j)
+            result.append(cnt)
+
+print(len(result))
+for i in sorted(result):
+    print(i)
