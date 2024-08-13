c924e00b1c8674f7577f84afcaff7a37fe20c1af
seheonnn <ho78901@naver.com>
2024-08-13 17:46:31 +0900

    DP - 정수 사각형 최소 합

--- codetree/DP/정수 사각형 최소 합.py
+++ codetree/DP/정수 사각형 최소 합.py
@@ -9,13 +9,14 @@ def initialize():
     # DP 테이블 초기화 (시작점)
     dp[0][n-1] = a[0][n-1]
 
+    # 첫 번째 열 초기화 (위에서 아래로)
+    # [i][0]이 아닌 [i][n-1]인 이유는 시작점이 0, n-1이기 때문
+    for i in range(1, n):
+        dp[i][n - 1] = dp[i - 1][n - 1] + a[i][n - 1]
+
     # 첫 번째 행 초기화 (오른쪽에서 왼쪽으로)
     for j in range(n-2, -1, -1):
         dp[0][j] = dp[0][j+1] + a[0][j]
-
-    # 첫 번째 열 초기화 (위에서 아래로)
-    for i in range(1, n):
-        dp[i][n-1] = dp[i-1][n-1] + a[i][n-1]
 
 initialize()
 
