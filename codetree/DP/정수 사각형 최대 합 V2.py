946022efd9d9f9ebfd9599b987130acabbd6b972
seheonnn <ho78901@naver.com>
2024-08-15 13:25:18 +0900

    DP - 정수 사각형 최대 합 복습

---
+++ codetree/DP/정수 사각형 최대 합 V2.py
@@ -0,0 +1,23 @@
+# 복습
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+
+def initialize():
+    dp[0][0] = a[0][0]
+    for i in range(1, n):
+        dp[i][0] = dp[i-1][0] + a[i][0]
+    for j in range(1, n):
+        dp[0][j] = dp[0][j-1] + a[0][j]
+
+initialize()
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + a[i][j]
+
+print(dp[n-1][n-1])
