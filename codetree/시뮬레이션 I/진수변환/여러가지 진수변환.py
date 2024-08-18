4e5dbcb46dd8006d1f6766fd4d7fba99fa6f7413
seheonnn <ho78901@naver.com>
2024-08-18 12:51:12 +0900

    시뮬레이션 - 여러가지 진수변환

---
+++ codetree/시뮬레이션 I/진수변환/여러가지 진수변환.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, b = map(int, input().split())
+digits = []
+
+while True:
+    if n < b:
+        digits.append(n)
+        break
+
+    digits.append(n % b)
+    n //= b
+
+for i in digits[::-1]:
+    print(i,end="")
