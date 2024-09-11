b25cdeb163f90264737e9a6b01cb44ae6a7f58ae
seheonnn <ho78901@naver.com>
2024-09-11 18:33:39 +0900

    완전탐색1 - 일렬로 서있는 소 2

---
+++ codetree/완전탐색 1/일렬로 서있는 소2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if lst[i] <= lst[j] <= lst[k]:
+                cnt += 1
+print(cnt)
