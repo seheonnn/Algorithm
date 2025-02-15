a2f9e6d09815199b5a835b52a61dbe600d91d364
seheonnn <ho78901@naver.com>
2025-02-15 15:43:00 +0900

    수열 - N과 M(1)

---
+++ BOJ/수열/15649-N과 M(1).java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static ArrayList<Integer> ans;
+    static int[] visited;
+    public static void backtracking() {
+        if (ans.size() == m) {
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                ans.add(i);
+                backtracking();
+                ans.remove(ans.size() - 1);
+                visited[i] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        ans = new ArrayList<>();
+        visited = new int[n + 1];
+        backtracking();
+    }
+}
