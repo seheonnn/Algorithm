58102cf0cf7b01725e3ee806c531618912d7ed92
seheonnn <ho78901@naver.com>
2025-02-25 21:11:05 +0900

    DP - 동전 1

--- BOJ/Dynamic Programming/2293-동전 1.cpp
+++ BOJ/Dynamic Programming/2293-동전 1.cpp
@@ -1,28 +1,27 @@
-// 백준 2293 동전 1
 #include <iostream>
-#include <climits>
-#define MAX 10001
+#include <vector>
 
 using namespace std;
 
 int n, k;
-int coin[101];
-int dp[MAX]; // dp[i] : i원을 만드는 경우의 수
+vector<int> coins;
+vector<int> dp; // dp[i] : i원을 만드는 경우의 수
 
 int main() {
-    scanf("%d %d", &n, &k);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", coin + i);
+    cin >> n >> k;
+    coins.assign(n + 1, 0);
+    dp.assign(k + 1, 0);
+    for (int i = 1; i <= n; i++){
+        cin >> coins[i];
     }
 
     dp[0] = 1;
-    for (int i = 0; i < n; i++) {
-        for (int j = coin[i]; j <= k; j++) {
-            dp[j] += dp[j - coin[i]]; // 동전 coin[i]을 사용하여 j원을 만들 수 있는 경우싀 수
+    for (int i = 1; i <= n; i++) {
+        for (int j = coins[i]; j <= k; j++) {
+            dp[j] += dp[j - coins[i]];
         }
     }
 
-    printf("%d\n", dp[k]);
-
+    cout << dp[k] << endl;
     return 0;
 }
