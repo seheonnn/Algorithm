75f44a3502a0f3db9654acbb715de7581731cb36
seheonnn <ho78901@naver.com>
2025-03-02 23:34:36 +0900

    BFS - 트리의 부모 찾

---
+++ BOJ/BFS/11725-트리의 부모 찾기.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static ArrayList<Integer>[] graph;
+    static int[] visited;
+    public static void BFS(int v) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(v);
+
+        while(!pq.isEmpty()) {
+            int cur = pq.poll();
+            for (int next : graph[cur]) {
+                if (visited[next] == -1) {
+                    visited[next] = cur;
+                    pq.add(next);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[n + 1];
+        visited = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+            visited[i] = -1;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int a, b;
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        visited[1] = 0;
+        BFS(1);
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(visited[i]);
+        }
+    }
+}
