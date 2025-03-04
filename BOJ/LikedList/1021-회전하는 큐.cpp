31cd7d23763ee66c524bc21047ebe58f69e7aa09
seheonnn <ho78901@naver.com>
2025-03-04 20:12:54 +0900

    원형 큐 - 회전하는 큐

--- BOJ/LikedList/1021-회전하는 큐.cpp
+++ BOJ/LikedList/1021-회전하는 큐.cpp
@@ -1,54 +1,46 @@
-// 백준 1021 - 회전하는 큐
+// 백준 1021 회전하는 큐 회전 큐
 #include <iostream>
 #include <deque>
-#include <algorithm>
-
-#define MAX 50
 
 using namespace std;
 
+int n, m, x, cnt;
+deque<int> queue;
+
 int main() {
-    int n, m;
     cin >> n >> m;
-
-    deque<int> queue;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) {
         queue.push_back(i);
+    }
 
-    int arr[MAX];
-    for (int i = 0; i < m; i++)
-        cin >> arr[i];
+    while(m--) {
+        cin >> x;
 
-    int cnt = 0;
-    for (int i = 0; i < m; i++) {
-        // 현재 위치를 찾음
-        auto it = find(queue.begin(), queue.end(), arr[i]);
-        int idx = distance(queue.begin(), it);
-
-        // 좌/우 방향 계산
-        int left = idx;
-        int right = queue.size() - idx;
-
-        // 적은 회전 방향 선택
-        if (left <= right) {
-            for (int j = 0; j < left; j++) {
+        // x가 현재 deque에서 몇 번째 인덱스인지 찾기
+        int idx = 0;
+        for (int i = 0; i < queue.size(); i++) {
+            if (queue[i] == x) {
+                idx = i;
+                break;
+            }
+        }
+        while(1) {
+            if (queue.front() == x) {
+                queue.pop_front();
+                break;
+            } else if (idx < queue.size() - idx) {
                 queue.push_back(queue.front());
                 queue.pop_front();
-            }
-            cnt += left;
-        }
-        else {
-            for (int j = 0; j < right; j++) {
+                cnt++;
+            } else {
                 queue.push_front(queue.back());
                 queue.pop_back();
+                cnt++;
             }
-            cnt += right;
         }
-
-        // 현재 값을 제거
-        queue.pop_front();
     }
 
     cout << cnt << endl;
+
     return 0;
 }
