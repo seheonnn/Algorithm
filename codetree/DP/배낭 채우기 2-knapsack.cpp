5c6e1de3116c7f2d26adb7c7d0850e77448cf08d
seheonnn <ho78901@naver.com>
2025-04-15 22:52:53 +0900

    DP - 배낭 채우기 2

---
+++ codetree/DP/배낭 채우기 2-knapsack.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int n, m;
+vector<int> weights;
+vector<int> values;
+vector<int> dp;
+int main() {
+    cin >> n >> m;
+
+    weights.assign(n, 0);
+    values.assign(n, 0);
+    dp.assign(m + 1, 0);
+
+    for (int i = 0; i < n; i++) {
+        cin >> weights[i] >> values[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = weights[i]; j <= m; j++) {  // 중복 허용
+            dp[j] = max(dp[j], dp[j - weights[i]] + values[i]);
+        }
+    }
+
+    int r = INT_MIN;
+    for (int i = 0; i <= m; i++) {
+        r = max(r, dp[i]);
+    }
+
+    cout << r << endl;
+
+    return 0;
+}
