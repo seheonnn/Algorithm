426be963c7de1579044e89b2bee7982d2b7ad5f9
seheonnn <ho78901@naver.com>
2025-06-28 15:25:58 +0900

    Multiset - 이중우선순위 큐

--- Programmers/우선순위 큐/이중우선순위 큐.cpp
+++ Programmers/우선순위 큐/이중우선순위 큐.cpp
@@ -1,28 +1,54 @@
-// 프로그래머스 이중우선순위큐
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// vector<int> solution(vector<string> operations) {
+//     vector<int> queue;
+
+//     for (const auto& op : operations) {
+//         if (op[0] == 'I') {
+//             queue.push_back(stoi(op.substr(2)));
+//             sort(queue.begin(), queue.end());
+//         } else if(!queue.empty()) {
+//             if (op == "D 1") {
+//                 queue.erase(queue.end() - 1);
+//             } else {
+//                 queue.erase(queue.begin());
+//             }
+//         }
+//     }
+
+//     if (queue.empty()) return {0, 0};
+//     return {queue.back(), queue.front()};
+// }
+
+
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <set>
 
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    vector<int> queue;
+    multiset<int> ms;
 
-    for (const auto& op : operations) {
+    for (const string& op : operations) {
         if (op[0] == 'I') {
-            queue.push_back(stoi(op.substr(2)));
-            sort(queue.begin(), queue.end());
-        }
-        else if (!queue.empty()) {
-            if (op == "D 1") {
-                queue.erase(queue.end() - 1);
-            }
-            else if (op == "D -1") {
-                queue.erase(queue.begin());
+            int num = stoi(op.substr(2));
+            ms.insert(num);
+        } else if (!ms.empty()) {
+            if (op[2] == '1') {
+                // erase 최대값
+                ms.erase(prev(ms.end())); // prev(ms.end()) == ms.end() - 1 in Multiset
+            } else {
+                // erase 최소값
+                ms.erase(ms.begin());
             }
         }
     }
 
-    if (queue.empty()) return {0, 0};
-    return {queue.back(), queue.front()};
+    if (ms.empty()) return {0, 0};
+    return {*prev(ms.end()), *ms.begin()};
 }
