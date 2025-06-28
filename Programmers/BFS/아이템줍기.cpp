c5814805fcbea240772c8ef66e8b241d7689819c
seheonnn <ho78901@naver.com>
2025-06-28 15:26:12 +0900

    BFS - 아이템줍기

--- Programmers/BFS/아이템줍기.cpp
+++ Programmers/BFS/아이템줍기.cpp
@@ -1,66 +1,68 @@
-// 프로그래머스 아이템줍기 BFS
+#include <string>
 #include <vector>
-#include <deque>
+#include <cstring>
+#include <queue>
+
+#define MAX 101
 
 using namespace std;
 
-int graph[101][101];
-int visited[101][101];
-int dx[] = {0, -1, 0, 1};
-int dy[] = {-1, 0, 1, 0};
-int answer;
+int graph[MAX][MAX];
+int visited[MAX][MAX];
+int dx[4] = {0, -1, 0, 1};
+int dy[4] = {-1, 0, 1, 0};
+int answer = 0;
 
-void BFS(int x, int y, int ix, int iy) {
-    deque<pair<int, int>> queue;
-    queue.push_back({x, y});
+void BFS(int x, int y, int itemX, int itemY) {
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    visited[x][y] = 1;
 
-    while(!queue.empty()) {
-        x = queue.front().first;
-        y = queue.front().second;
-        queue.pop_front();
+    while(!q.empty()) {
+        auto tmp = q.front();
+        x = tmp.first;
+        y = tmp.second;
+        q.pop();
 
-        if (x == ix && y == iy) {
-            answer = visited[ix][iy]  / 2;
+        if (x == itemX && y == itemY) {
+            answer = visited[x][y] / 2;
             return;
         }
+
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx < 0 || 101 <= nx || ny < 0 || 101 <= ny) continue;
+
+            if (nx < 0 || MAX <= nx || ny < 0 || MAX <= ny) continue;
             if (graph[nx][ny] == 1 && visited[nx][ny] == 0) {
                 visited[nx][ny] = visited[x][y] + 1;
-                queue.push_back({nx, ny});
+                q.push({nx, ny});
             }
         }
     }
 }
 
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
+    memset(graph, -1, sizeof(graph));
 
-    for (int i = 0; i < 101; i++) {
-        for (int j = 0; j < 101; j++) {
-            graph[i][j] = -1;
-        }
-    }
+    for (auto& tmp : rectangle) {
 
-    for (auto tmp : rectangle) {
+        // 겹치는 테두리를 정확히 구분하기 위함
         int x1 = tmp[0] * 2;
         int y1 = tmp[1] * 2;
         int x2 = tmp[2] * 2;
         int y2 = tmp[3] * 2;
 
+        // 범위 주의
         for (int i = x1; i <= x2; i++) {
             for (int j = y1; j <= y2; j++) {
-                if (x1 < i && i < x2 && y1 < j && j < y2) {
-                    graph[i][j] = 0;
-                } else if (graph[i][j] != 0) {
-                    graph[i][j] = 1;
-                }
+                if (x1 < i && i < x2 && y1 < j && j < y2) graph[i][j] = 0; // 직사각형 내부인 경우
+                else if (graph[i][j] != 0) graph[i][j] = 1;
             }
         }
     }
 
-    visited[characterX * 2][characterY * 2] = 1;
+
     BFS(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
 
     return answer;
