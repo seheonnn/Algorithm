99b497e39b87bb149225e5ab418a6072cc785570
seheonnn <ho78901@naver.com>
2025-06-30 10:13:38 +0900

    BFS - 네트워크

--- Programmers/BFS/네트워크.cpp
+++ Programmers/BFS/네트워크.cpp
@@ -1,22 +1,27 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-vector<int> visited;
+// 110
+// 110
+// 001
 
-void BFS(int v, int n, vector<vector<int>> computers) {
-    priority_queue<int> pq;
-    pq.push(v);
+vector<bool> visited(3);
+void BFS(int start, int n, vector<vector<int>> computers) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
 
-    while(!pq.empty()) {
-        v = pq.top();
-        pq.pop();
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
 
-        for (int next = 0; next < n; next++) {
-            if (computers[v][next] == 1 && visited[next] == 0) {
-                visited[next] = 1;
-                pq.push(next);
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && computers[v][i]) {
+                visited[i] = true;
+                q.push(i);
             }
         }
     }
@@ -24,10 +29,8 @@ void BFS(int v, int n, vector<vector<int>> computers) {
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    visited.assign(n, 0);
     for (int i = 0; i < n; i++) {
-        if (visited[i] == 0) {
-            visited[i] = 1;
+        if (!visited[i]) {
             BFS(i, n, computers);
             answer++;
         }
