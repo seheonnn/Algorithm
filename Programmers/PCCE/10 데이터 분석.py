cf8e2bee23b3dedc42552498fe58d6a9862abecd
seheonnn <ho78901@naver.com>
2025-09-05 15:00:17 +0900

    구현 - 데이터 분석

--- Programmers/PCCE/10 데이터 분석.py
+++ Programmers/PCCE/10 데이터 분석.py
@@ -1,11 +1,8 @@
 def solution(data, ext, val_ext, sort_by):
     answer = []
-
-    type_sort = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
-    data = sorted(data, key=lambda x: x[type_sort[sort_by]])
-
-    for v in data:
-        if v[type_sort[ext]] < val_ext:
-            answer.append(v)
-
+    dic = {"code" : 0, "date" : 1, "maximum" : 2, "remain" : 3}
+    data.sort(key=lambda x:x[dic[sort_by]])
+    for d in data:
+        if d[dic[ext]] < val_ext:
+            answer.append(d)
     return answer
