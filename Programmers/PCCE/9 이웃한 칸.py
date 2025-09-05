ea75d2e1cb149b8c211fe7f8cd1d8970d8f12457
seheonnn <ho78901@naver.com>
2025-09-05 15:07:43 +0900

    구현 - 이웃한 칸

--- Programmers/PCCE/9 이웃한 칸.py
+++ Programmers/PCCE/9 이웃한 칸.py
@@ -1,14 +1,12 @@
 def solution(board, h, w):
     answer = 0
+    dx, dy = [0, -1, 0, 1], [-1, 0, 1, 0]
     n = len(board)
-
-    dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+    m = len(board[0])
+    cnt = 0
     for i in range(4):
         nx, ny = h + dx[i], w + dy[i]
-
-        if nx < 0 or n <= nx or ny < 0 or n <= ny:
-            continue
+        if nx < 0 or n <= nx or ny < 0 or m <= ny: continue
         if board[h][w] == board[nx][ny]:
             answer += 1
-
     return answer
